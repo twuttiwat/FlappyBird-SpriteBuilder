a4e5b90aac396db9ebe382d5f30b5ed8d118dae3
Teerawat Wuttiwat <teerawat@wuttiwat.com>
2014-12-19 23:26:48 +0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -15,6 +15,9 @@
     // put update code here
 }
 
-// put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
+}
 
 @end
